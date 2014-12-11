d98e12f55ecb4ec5ec7e5cdde7fe0aa459176f15
Caleb Tutty <caleb@carnivallabs.com>
2014-12-12 11:46:30 +1300

    Added a last modified property

--- lib/nz_mps_popolo/rss_parser.rb
+++ lib/nz_mps_popolo/rss_parser.rb
@@ -1,13 +1,16 @@
 module NZMPsPopolo
   class RSSParser
+    attr_reader :feed, :last_modified
+
     URL = 'http://www.parliament.nz/en-nz/syndication?posting=/en-nz/mpp/mps/current/'
 
     def parse
-      Feedjira::Feed.fetch_and_parse URL
+      @feed = Feedjira::Feed.fetch_and_parse URL
     end
 
     def call
       parse
+      @last_modified = feed.last_modified
     end
   end
 end
--- spec/rss_parser_spec.rb
+++ spec/rss_parser_spec.rb
@@ -2,8 +2,11 @@ require 'spec_helper'
 
 describe NZMPsPopolo::RSSParser do
   it 'returns an RSS feed' do
-    expect(subject.call).to be_a Feedjira::Parser::Atom
+    expect(subject.parse).to be_a Feedjira::Parser::Atom
   end
 
-  it 'has a last_modified property'
+  it 'has a last_modified property' do
+    subject.call
+    expect(subject.last_modified).to_not be_nil
+  end
 end
