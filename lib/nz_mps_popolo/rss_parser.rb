6063b8e2148837cfc13dc72266c3b128e8aa8708
Caleb Tutty <caleb@carnivallabs.com>
2014-12-12 17:04:17 +1300

    Parse to an array of MP objects

--- lib/nz_mps_popolo/rss_parser.rb
+++ lib/nz_mps_popolo/rss_parser.rb
@@ -1,16 +1,44 @@
 module NZMPsPopolo
   class RSSParser
-    attr_reader :feed, :last_modified
+    attr_reader :feed, :last_modified, :mps
 
     URL = 'http://www.parliament.nz/en-nz/syndication?posting=/en-nz/mpp/mps/current/'
 
     def parse
       @feed = Feedjira::Feed.fetch_and_parse URL
+      @mps = generate_from_feed
     end
 
     def call
       parse
       @last_modified = feed.last_modified
+    end
+
+    private
+
+    def parse_party_and_electorate(electorate_details)
+      party, electorate = electorate_details.split(',').map(&:strip)
+      if electorate == 'List'
+        { electorate: nil, party: party, list: true }
+      else
+        { electorate: electorate, party: party, list: false }
+      end
+    end
+
+    def generate_from_feed
+      feed.entries.map do |item|
+        MP.new(parse_feed_item(item))
+      end
+    end
+
+    def parse_feed_item(item)
+      last_name, first_names = item.title.split(',').map(&:strip)
+      {
+        entry_id: item.entry_id,
+        first_names: first_names,
+        last_name: last_name,
+        details_url: item.links.first
+      }.merge(parse_party_and_electorate(item.content))
     end
   end
 end
--- spec/rss_parser_spec.rb
+++ spec/rss_parser_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe NZMPsPopolo::RSSParser do
+  before { subject.call }
+
   it 'returns an RSS feed' do
-    expect(subject.parse).to be_a Feedjira::Parser::Atom
+    expect(subject.feed).to be_a Feedjira::Parser::Atom
   end
 
   it 'has a last_modified property' do
-    subject.call
     expect(subject.last_modified).to_not be_nil
+  end
+
+  it 'holds an an array of MPs' do
+    expect(subject.mps).to be_a Array
+    expect(subject.mps.map(&:class).uniq).to eq [NZMPsPopolo::MP]
   end
 end
