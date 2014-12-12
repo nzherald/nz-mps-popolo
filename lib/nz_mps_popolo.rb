bb9b5aa4b67d2be9c36bf9a90c3623b16f1d7003
Caleb Tutty <caleb@carnivallabs.com>
2014-12-12 17:03:58 +1300

    Require MP class

--- lib/nz_mps_popolo.rb
+++ lib/nz_mps_popolo.rb
@@ -2,6 +2,7 @@ require 'bundler'
 Bundler.require
 
 require_relative './nz_mps_popolo/rss_parser'
+require_relative './nz_mps_popolo/mp'
 
 # New Zealand Members of Parliament RSS Parser
 # Scraper and Popolo spec formatter
