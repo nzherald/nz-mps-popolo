d7e091469f6c5cae38350a1eaaad745c0fb8c53f
Caleb Tutty <caleb@carnivallabs.com>
2014-12-15 21:18:25 +1300

    Party class

--- lib/nz_mps_popolo.rb
+++ lib/nz_mps_popolo.rb
@@ -3,11 +3,15 @@ Bundler.require
 
 require_relative './nz_mps_popolo/rss_parser'
 require_relative './nz_mps_popolo/mp'
+require_relative './nz_mps_popolo/party'
+
 
 # New Zealand Members of Parliament RSS Parser
 # Scraper and Popolo spec formatter
 module NZMPsPopolo
   def self.run
-    RSSParser.new.call
+    parser = RSSParser.new
+    parser.call
+
   end
 end
---
+++ spec/party_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe NZMPsPopolo::Party do
+  subject { NZMPsPopolo::Party }
+
+  it 'requires a name' do
+    expect { subject.new(wrong_key: 'foo') }.to raise_error KeyError
+  end
+
+  describe 'from_mps' do
+    let(:mp_1) { double }
+    let(:mp_2) { double }
+    let(:mp_3) { double }
+
+    it 'generates parties from an array of MPs' do
+      allow(mp_1).to receive(:party) { 'Apple' }
+      allow(mp_2).to receive(:party) { 'Pear' }
+      allow(mp_3).to receive(:party) { 'Orange' }
+
+      result = subject.from_mps([mp_1, mp_2, mp_3])
+
+      expect(result.map(&:class).uniq).to eq [NZMPsPopolo::Party]
+      expect(result[0].name).to eq 'Apple'
+      expect(result[1].name).to eq 'Pear'
+      expect(result[2].name).to eq 'Orange'
+    end
+  end
+end
