c7b028bc1c1de4f60356ba23016138e9a5ed725a
Caleb Tutty <caleb@carnivallabs.com>
2014-12-19 01:20:12 +1300

    Added more test helpers

---
+++ spec/support/mps.rb
@@ -0,0 +1,26 @@
+module NZMPsPopolo
+  module TestHelpers
+    def self.sample_mps
+      [
+        { name: 'Gerry Brownlee',
+          details_url: 'http://parliament.govt.nz/en-nz/mpp/mps/current/51MP141/brownlee-gerry' },
+        { name: 'Judith Collins',
+          details_url: 'http://parliament.govt.nz/en-nz/mpp/mps/current/51MP2021/collins-judith' },
+        { name: 'Marama Fox',
+          details_url: 'http://parliament.govt.nz/en-nz/mpp/mps/current/51MP3451/fox-marama' },
+        { name: 'Phil Goff',
+          details_url: 'http://parliament.govt.nz/en-nz/mpp/mps/current/51MP381/goff-phil' },
+        { name: 'Chris Hipkins',
+          details_url: 'http://parliament.govt.nz/en-nz/mpp/mps/current/51MP2871/hipkins-chris' },
+        { name: 'Tracey Martin',
+          details_url: 'http://parliament.govt.nz/en-nz/mpp/mps/current/51MP3211/martin-tracey' },
+        { name: 'Simon O\'Connor',
+          details_url: 'http://parliament.govt.nz/en-nz/mpp/mps/current/51MP3301/oconnor-simon' },
+        { name: 'Fletcher Tabuteau',
+          details_url: 'http://parliament.govt.nz/en-nz/mpp/mps/current/51MP3561/tabuteau-fletcher' },
+        { name: 'Megan Woods',
+          details_url: 'http://parliament.govt.nz/en-nz/mpp/mps/current/51MP3351/woods-megan' }
+      ]
+    end
+  end
+end
